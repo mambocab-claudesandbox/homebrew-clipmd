95504f45f0d98bd1eba7c20f2f1fb60cec09d60e
Jim Witschey <jim.witschey@gmail.com>
2026-06-09 21:30:12 -0400

    clipmd 0.1.1: menu-click segfault fix + status-icon auto-revert

--- Formula/clipmd@0.1.1.rb
+++ Formula/clipmd@0.1.1.rb
@@ -1,14 +1,10 @@
-class Clipmd < Formula
+class ClipmdAT011 < Formula
   desc "Transform clipboard URLs into Markdown links and rich-text hyperlinks"
   homepage "https://github.com/mambocab-claudesandbox/clipmd"
   url "https://github.com/mambocab-claudesandbox/clipmd/archive/refs/tags/v0.1.1.tar.gz"
   sha256 "aecbc8596efaecdf82c142c8e5116c9e8b7207304b8476e5854227deafd984f4"
   license "MIT"
-  head "https://github.com/mambocab-claudesandbox/clipmd.git", branch: "main"
 
-  # clipmd uses Zig 0.16 (std.zon, std.Io). `zig@0.16` is currently a
-  # homebrew-core alias for `zig`; when `zig` advances to 0.17 a real
-  # versioned formula will appear and we'll still pull in 0.16.
   depends_on "zig@0.16" => :build
 
   def install
